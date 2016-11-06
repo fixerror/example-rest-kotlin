bf562d479b3b9bbef8149b8b93911c512ec605dd
FixError <ra7777777>
2016-11-06 18:03:34 +0200

    start server wasabi

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,6 +4,8 @@ package main
 /**
  * Created by FixError on 02.11.2016.
  */
+import org.wasabifx.wasabi.app.AppConfiguration
+import org.wasabifx.wasabi.app.AppServer
 import models.Test1
 import models.Test2
 
@@ -12,4 +14,12 @@ fun main(args: Array<String>){
     val t = Test1()
     t.test1()
     Test2.test();
+
+    val server = AppServer(AppConfiguration(enableLogging = false))
+
+    server.get("/", {
+        response.send("Hello World!")
+    })
+
+    server.start()
 }
